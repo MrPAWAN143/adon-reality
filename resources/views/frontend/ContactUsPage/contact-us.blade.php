1f4a5c05e6a45042d5533709cc68d9a28395dff1
Piyushchauhan10 <piyushrajput1020@gmail.com>
2025-06-20 14:43:59 +0530

    initial commit

--- resources/views/frontend/ContactUsPage/contact-us.blade.php
+++ resources/views/frontend/ContactUsPage/contact-us.blade.php
@@ -8,9 +8,41 @@
 
 @section('content')
 <h1>Contact Us page</h1>
+<x-forkawesome-instagram class='text-xl h-6 w-6'  />
+
+@include('frontend.ContactUsPage.testimonials')
+
 @endsection
 
 
 @section('scripts')
+<script type="module">
+    $(document).ready(function() {
+        $('.testimonial-slider').slick({
+            centerMode: true,
+            centerPadding: '60px',
+            slidesToShow: 3,
+            responsive: [{
+                    breakpoint: 768,
+                    settings: {
+                        arrows: false,
+                        centerMode: true,
+                        centerPadding: '40px',
+                        slidesToShow: 3
+                    }
+                },
+                {
+                    breakpoint: 480,
+                    settings: {
+                        arrows: false,
+                        centerMode: true,
+                        centerPadding: '40px',
+                        slidesToShow: 1
+                    }
+                }
+            ]
+        });
+    });
+</script>
 
 @endsection
